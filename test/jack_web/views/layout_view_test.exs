0821a49ca6430cac3d40829d4cbea62656dec637
Jack <luis.6496@hotmail.com>
2021-06-26 02:00:18 -0500

    Initial commit

---
+++ test/jack_web/views/layout_view_test.exs
@@ -0,0 +1,8 @@
+defmodule JackWeb.LayoutViewTest do
+  use JackWeb.ConnCase, async: true
+
+  # When testing helpers, you may want to import Phoenix.HTML and
+  # use functions such as safe_to_string() to convert the helper
+  # result into an HTML string.
+  # import Phoenix.HTML
+end
