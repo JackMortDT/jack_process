f2d84b5cf24e7b96a2099a90d0d3136b5741851a
Jack <luis.6496@hotmail.com>
2021-06-26 14:10:59 -0500

    #1 Adding xlsx and mogrify dependency

--- mix.exs
+++ mix.exs
@@ -44,7 +44,9 @@ defmodule Jack.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:mogrify, "~> 0.8.0"},
+      {:xlsxir, "~> 1.6"},
     ]
   end
 
