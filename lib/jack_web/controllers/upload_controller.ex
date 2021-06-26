aca4e3dc779789a2695ccabdb9d800678d660035
Jack <luis.6496@hotmail.com>
2021-06-26 02:39:39 -0500

    Making controller for upload and list files

--- lib/jack_web/controllers/upload_controller.ex
+++ lib/jack_web/controllers/upload_controller.ex
@@ -1,12 +1,25 @@
 defmodule JackWeb.UploadController do
   use JackWeb, :controller
 
+  alias Jack.Documents
+
+  def index(conn, _params) do
+    uploads = Documents.list_uploads()
+    render(conn, "index.html", uploads: uploads)
+  end
+
   def new(conn, _params) do
     render(conn, "new.html")
   end
 
   def create(conn, %{"upload" => %Plug.Upload{}=upload}) do
-    IO.inspect(upload, label: "upload")
-    text conn, "ok"
+    case Documents.create_upload_from_plug_upload(upload) do
+      {:ok, _upload} ->
+        put_flash(conn, :info, "file uploaded correctly")
+        redirect(conn, to: Routes.upload_path(conn, :index))
+      {:error, reason} ->
+        put_flash(conn, :error, "error upload file: #{inspect(reason)}")
+        render(conn, "new.html")
+    end
   end
 end
